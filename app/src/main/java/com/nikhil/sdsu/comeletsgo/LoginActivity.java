25dd8d924a58c41d820802548316b91c0175b5e9
Nikhil <nikhil0689@gmail.com>
2017-12-22 18:58:25 -0500

    test commit

--- app/src/main/java/com/nikhil/sdsu/comeletsgo/LoginActivity.java
+++ app/src/main/java/com/nikhil/sdsu/comeletsgo/LoginActivity.java
@@ -1,13 +1,93 @@
 package com.nikhil.sdsu.comeletsgo;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText emailId;
+    private EditText password;
+    private Button signIn;
+    private Button signUp;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailId =  findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        signIn = findViewById(R.id.login_submit);
+        signUp = findViewById(R.id.login_sign_up);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpActivity = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(signUpActivity);
+                finish();
+            }
+        });
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validInput()){
+                    authenticateUser(emailId.getText().toString().trim(),password.getText().toString().trim());
+                }
+            }
+
+        });
+    }
+
+    private void authenticateUser(String email, String pass) {
+        Log.d("","Authenticating user");
+        firebaseAuth.signInWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Invalid email or password", Toast.LENGTH_LONG).show();
+                        } else {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+    }
+
+    private boolean validInput() {
+        boolean valid = true;
+        if(TextUtils.isEmpty(emailId.getText().toString())){
+            emailId.setError("Enter Email");
+            valid = false;
+        }
+        if (TextUtils.isEmpty(password.getText().toString())) {
+            password.setError("Enter Password");
+            valid = false;
+        }
+
+        if (password.getText().toString().length() < 6) {
+            password.setError("Password length is less than 6");
+            valid = false;
+        }
+        return valid;
     }
 }
---
+++ app/src/main/java/com/nikhil/sdsu/comeletsgo/SignUpActivity.java
@@ -0,0 +1,129 @@
+package com.nikhil.sdsu.comeletsgo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class SignUpActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText contact;
+    private EditText emailId;
+    private EditText password;
+    private Button submit,reset;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        name = findViewById(R.id.sign_up_name);
+        contact = findViewById(R.id.sign_up_contact);
+        emailId = findViewById(R.id.sign_up_email);
+        password = findViewById(R.id.signup_password);
+        submit = findViewById(R.id.sign_up_submit);
+        reset = findViewById(R.id.sign_up_reset_button);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = emailId.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                String dispName = name.getText().toString().trim();
+                String phno = contact.getText().toString().trim();
+                final String displayName = phno.concat("-").concat(dispName);
+                if(validInput()){
+                    Log.d("rew","user data: "+email+" "+pass+" "+displayName);
+                    auth.createUserWithEmailAndPassword(email,pass)
+                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(SignUpActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                                        emailId.setError("Invalid Email ID");
+                                    } else {
+                                        FirebaseUser user = auth.getCurrentUser();
+
+                                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                                .setDisplayName(displayName)
+                                                .build();
+                                        try {
+                                            user.updateProfile(profileUpdates)
+                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            if (task.isSuccessful()) {
+                                                                Log.d("rew", "User profile updated.");
+                                                                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                                                                finish();
+                                                            }
+                                                        }
+                                                    });
+                                        } catch (NullPointerException e) {
+                                            Log.d("rew", "failed");
+                                        }
+                                    }
+                                }
+                            });
+                }else{
+                    Toast.makeText(getApplicationContext(), "Enter data",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailId.setText("");
+                password.setText("");
+                name.setText("");
+                contact.setText("");
+            }
+        });
+    }
+
+    private boolean validInput() {
+        boolean dataValid = true;
+        if (TextUtils.isEmpty(emailId.getText().toString())) {
+            emailId.setError("Enter Email ID");
+            dataValid = false;
+        }
+        if (TextUtils.isEmpty(password.getText().toString())) {
+            password.setError("Enter Password");
+            dataValid = false;
+        }
+
+        if (password.getText().toString().length() < 6) {
+            password.setError("Invalid Password");
+            Toast.makeText(getApplicationContext(), "Min 6 Characters",
+                    Toast.LENGTH_SHORT).show();
+            dataValid = false;
+        }
+
+        if(TextUtils.isEmpty(name.getText().toString())){
+            name.setError("Enter name");
+            dataValid = false;
+        }
+        if(name.getText().toString().matches(".*\\d+.*")){
+            name.setError("No Numbers please");
+            dataValid = false;
+        }
+        if(TextUtils.isEmpty(contact.getText().toString())){
+            contact.setError("Enter Phone No.");
+            dataValid = false;
+        }
+        return dataValid;
+    }
+
+}
